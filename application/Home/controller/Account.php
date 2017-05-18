40d7ba36dbfd2206d59d4e16cb3228d63f8a962a
leeprincehz <leeprincehz@gmail.com>
2017-05-18 16:11:30 +0800

    new file

---
+++ application/Home/controller/Account.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\Home\controller;
+
+class Account
+{
+
+}
--- build.php
+++ build.php
@@ -14,12 +14,12 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
-        '__file__'   => ['common.php'],
+    'Home'     => [
+        '__file__'   => ['common.php','route.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
-        'view'       => ['index/index'],
+        'controller' => ['Index','Account'],
+        'model'      => [],
+        'view'       => ['index/index','account/accoutn'],
     ],
     // 其他更多的模块定义
 ];
