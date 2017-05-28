0c1779dbea3dc5dd358e762604f0c68873286bec
leeprince <1048455252@qq.com>
2017-05-29 01:13:16 +0800

    buyer sign up function successful

--- application/Home/controller/Account.php
+++ application/Home/controller/Account.php
@@ -3,9 +3,7 @@ namespace app\home\controller;
 
 use \think\Controller;
 use \think\Request;//Request::instance();
-use app\home\model\CheckAccountModel;
-use \think\Db;
-use \think\Model;
+use app\home\model\AccountModel;
 // use \think\Debug;//Request::instance();
 
 
@@ -19,12 +17,16 @@ class Account extends Controller
 		if($request->isPost()){
 
 		}else{
+			// trace(session('BUYERID').' | '.session('BUYEREMAIL'),'debug');
 			// return view();//助手函数
 			return $this->fetch();
 		}
 	}
 
 	// 注册
+	// profileUrl:https://www.amazon.com/gp/profile/A3OAEFRIYGNK0L
+	// profileUrl:https://www.amazon.com/gp/profile/amzn1.account.AEX4YHNYKDFGWFXR67PX2FVTX7KQ
+	// profileID:AEX4YHNYKDFGWFXR67PX2FVTX7KQ
 	public function signup()
 	{
 		$request = Request::instance();
@@ -42,7 +44,7 @@ class Account extends Controller
 			}
 			$data['ipAddress'] = session('ipAddress');
 			$data['profileID'] = session('profileID');
-			$data['channel'] = session('CH');
+			$data['channel'] = session('?CH')?session('CH'):'';
 			$data['rank'] = 3;
 			$data['points'] = 0;
 			$data['paypalAccount'] = '';
@@ -52,13 +54,29 @@ class Account extends Controller
 			$data['reviewRebate'] = 'like';
 			$data['registerDate'] = date('Y-m-d H:i:s');
 			$data['activeCode'] = md5(md5(rand(000,999)).md5($data['buyerEmail']));
-			// session(null);
+			session(null);
 			// halt($shortProfileID.$profileID);
-			$signup_save = new AccountModel();
-
-
-
-
+			$model = new AccountModel();
+			$InsID = $model -> insSignupData($data);
+			if($InsID){
+				$to = "leeprince@foxmail.com";
+				$subject = "Sign Up Successful.";
+				$body = "buyerID:$InsID\n
+						buyerEmail:$data['buyerEmail']\n
+						profileID:$profileID"
+				    	.config('EMAILABOUNT'); // HTML  tags
+				$emailReturn = send_emial_163($to,$subject,$body);//163邮箱发送邮件
+				// halt('insSignupData successful');
+				session('BUYERID',$InsID);
+				session('BUYEREMAIL',$data['buyerEmail']);
+				// halt(session('BUYERID'));
+				// trace(session('BUYERID'),'debug');
+				// $this->redirect('Account/signin');
+				$this->success('registration success!');
+			}else{
+				// halt('insSignupData Fail');
+				$this->error('registration failed!');
+			}
 
 		}else{
 			// return view();//助手函数
@@ -72,13 +90,12 @@ class Account extends Controller
 
 		$request = Request::instance();
 		$buyerEmail = $request->post('account');
-		$model = new CheckAccountModel();
+		$model = new AccountModel();
 
 		if(!empty($buyerEmail)){
 			$ipAddress = getRealIp();
 			session('ipAddress',$ipAddress);
 
-			$data = [];
 			$data = [
 				'ip'=>[
 					'ipAddress' => $ipAddress,
@@ -93,7 +110,7 @@ class Account extends Controller
 
 			if($isIpExist){
 				$check = $model::IP_EXIST;
-				// $check = CheckAccountModel::IP_EXIST;
+				// $check = AccountModel::IP_EXIST;
 			}else{
 				if($isEmailExist){
 					$check = $model::EMAIL_EXIST;
@@ -114,7 +131,7 @@ class Account extends Controller
 	{
 		$request = Request::instance();
 		$profile = $request->post('profileUrl');
-		$model 	 = new CheckAccountModel();
+		$model 	 = new AccountModel();
 
 		// profileUrl:https://www.amazon.com/gp/profile/A3OAEFRIYGNK0L
 		// profileUrl:https://www.amazon.com/gp/profile/amzn1.account.AEX4YHNYKDFGWFXR67PX2FVTX7KQ
@@ -138,7 +155,6 @@ class Account extends Controller
 				$profileID = str_replace('/profile/amzn1.account.','',$matches['0']);
 				session('profileID',$profileID);
 
-				$data = [];
 				$data = [
 					'profileID' => $profileID,
 				];
